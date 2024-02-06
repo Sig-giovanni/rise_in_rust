6ab4d79829cb95a005613cad2011c544eac3dc60
unknown <kruizkk@gmail.com>
2024-02-06 14:02:14 +0100

    done calculation exercise

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,40 @@ fn main() {
     let concatenated_string = concatenate_strings( string_a, &string_b);
 
     println!("{}", concatenated_string);
+
+    let calculation = Operation::Add{ a: 5.0, b: 6.0 };
+    let calculation_result = calculate_operation(calculation);
 }
 
+//Exercise One
 fn concatenate_strings(s1:  String, s2:  &String)  -> String {
    let mut result = s1.clone();
     result.push_str(s2);
     result
+}
+
+
+//Exercise Two
+enum Operation{
+    Add{a: f64, b: f64},
+    Subtract{a: f64, b: f64},
+    Multiply{a: f64, b: f64},
+    Divide{a: f64, b: f64},
+}
+
+fn calculate_operation(operand: Operation) {
+    match operand {
+        Operation::Add { a, b } => {
+            println!("{} + {} = {}", a, b, a + b);
+        }
+        Operation::Subtract { a, b } => {
+            println!("{} - {} = {}", a, b, a - b);
+    }
+    Operation::Multiply { a, b } => {
+        println!("{} * {} = {}", a, b, a * b)
+    }
+    Operation::Divide { a, b } => {
+        println!("{} / {} = {}", a, b, a / b);
+    }
+}
 }
